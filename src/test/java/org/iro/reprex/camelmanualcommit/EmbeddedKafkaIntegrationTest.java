41a11ec93873395087d0b98a8b5c069d801e9d03
IVAN RODODENDRO <ivan.rododendro@acoss.fr>
2022-11-22 14:37:39 +0000

    first commit

---
+++ src/test/java/org/iro/reprex/camelmanualcommit/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,45 @@
+package org.iro.reprex.camelmanualcommit;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+public class EmbeddedKafkaIntegrationTest {
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingWithSimpleProducer_thenMessageReceived() 
+      throws Exception {
+    	// given 
+    	// when
+        String data = "Sending with our own simple KafkaProducer";
+        producer.send(topic, data);
+        
+        boolean messageConsumed = consumer.getLatch().await(10, TimeUnit.SECONDS);
+        assertTrue(messageConsumed);
+        assertThat(consumer.getPayload(), containsString(data));
+    }
+}
